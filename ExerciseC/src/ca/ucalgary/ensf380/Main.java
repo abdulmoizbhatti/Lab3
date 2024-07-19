eda45a28f0e1e77282eba9007865d11cb87c07a3
abdulmoizbhatti <abdulmoiz.bhatti@ucalgary.ca>
2024-07-18 20:48:12 -0600

    End of Section C

---
+++ ExerciseC/src/ca/ucalgary/ensf380/Main.java
@@ -0,0 +1,25 @@
+package ca.ucalgary.ensf380;
+
+public class Main {
+    public static void main(String[] args) {
+        Address professorAddress = new Address("118 Dark Street", "Calgary", "Alberta", "T33 ABC", "Canada");
+        Address studentAddress = new Address("21 Steel Street", "Calgary", "Alberta", "T36 CBA", "Canada");
+        Professor professor = new Professor("Dr. Stark Wayne", "403-222-3333", "swayne@gmail.com", professorAddress, Person.nextPersonalNumber((short) 1), "T100", 60000.0);
+        Student student = new Student("Peter Parker", "403-543-5231", "pparker@gmail.com", studentAddress, Person.nextPersonalNumber((short) 2), "S14348001", professor, 90.0);
+        
+
+        System.out.println("Professor: " + professor.getName());
+        System.out.println("Professor's Teacher Number: " + professor.getTeacherNumber());
+        System.out.println("Professor's Salary: $" + professor.getSalary());
+        System.out.println("Professor's Address: " + professor.getAddress().outputAsLabel());
+        System.out.println("Is Professor's Address Valid? " + professor.hasValidAddress());
+        System.out.println();
+        System.out.println("Student: " + student.getName());
+        System.out.println("Student's Student Number: " + student.getStudentNumber());
+        System.out.println("Student's Average Mark: " + student.getAverageMark());
+        System.out.println("Is Student Eligible to Enroll? " + student.isEligibleToEnroll());
+        System.out.println("Student's Address: " + student.getAddress().outputAsLabel());
+        System.out.println("Is Student's Address Valid? " + student.hasValidAddress());
+        System.out.println("Student's Supervisor: " + student.getSupervisor().getName());
+    }
+}
---
+++ ExerciseC/src/ca/ucalgary/ensf380/Person.java
@@ -0,0 +1,65 @@
+package ca.ucalgary.ensf380;
+
+public abstract class Person {
+    private String name;
+    private String phoneNumber;
+    private String emailAddress;
+    private Address address;
+    private int lastPersonalNumber;
+
+    public Person(String name, String phoneNumber, String emailAddress, Address address, int lastPersonalNumber) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.emailAddress = emailAddress;
+        this.address = address;
+        this.lastPersonalNumber = lastPersonalNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public int getLastPersonalNumber() {
+        return lastPersonalNumber;
+    }
+
+    public void setLastPersonalNumber(int lastPersonalNumber) {
+        this.lastPersonalNumber = lastPersonalNumber;
+    }
+
+    public static int nextPersonalNumber(short type) {
+        return type * 100000 + (int) (System.currentTimeMillis() % 100000);
+    }
+
+    public boolean hasValidAddress() {
+        return address.isValidated();
+    }
+}
